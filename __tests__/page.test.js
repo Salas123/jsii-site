3646fd286057421b26cf0c60cb7df8afc44a2b86
Salas123 <salasii.jesus@gmail.com>
2024-01-04 21:38:00 -0800

    adding unit test jest framework

---
+++ __tests__/page.test.js
@@ -0,0 +1,20 @@
+import { render, screen} from "@testing-library/react";
+import {describe, it, expect} from "@jest/globals";
+import Home from '../src/app/page'
+
+describe('Page.js: Component Rendering', () => {
+    const expectedSectionHeaders =['Interests', 'Projects', 'Contact Me' ]
+    it('Test All Expected Section Headers', () => {
+        render(<Home/>)
+
+        const headers = screen.getAllByRole('heading',{
+            level:1
+        })
+
+        const elementTextVals = headers.map((element) => {
+           return element.innerHTML
+        });
+
+        expect(elementTextVals).toEqual(expect.arrayContaining(expectedSectionHeaders))
+    });
+});
